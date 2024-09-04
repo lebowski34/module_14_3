8e5ea1171b6ca55df8536a974a90447107151b00
lebowski34 <167763740+lebowski34@users.noreply.github.com>
2024-09-04 07:48:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 import os
 
-api = "6716646027:AAFOK2SN_sasZ0i_IZUTgxtyK7WAp-q4gL4"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
